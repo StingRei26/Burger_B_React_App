b2bed6351e4f3c5b52cd358bc48c9a922381cd2c
Reiniel Ravelo <reinielravelo@gmail.com>
2019-10-09 06:40:26 -0400

    updated logo

--- react_burger_app/src/components/Layout/Layout.js
+++ react_burger_app/src/components/Layout/Layout.js
@@ -3,11 +3,13 @@ import Aux  from '../../hoc/Aux'; // this is used to have multiple divs in one c
 import styles from './Layout.module.css'; // to pull all css classes from Layouts.css
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar'; 
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'; 
 
 
 const layout = (props) => (
    <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className={styles.content}>
         {props.children}
     </main>
--- react_burger_app/src/components/Navigation/Toolbar/Toolbar.js
+++ react_burger_app/src/components/Navigation/Toolbar/Toolbar.js
@@ -8,7 +8,7 @@ const toolbar = (props) => (
     <header className={classes.Toolbar}>
 
         <div>MENU</div>
-        <Logo />
+        <Logo height="80%"/>
         <nav>
         <NavigationItems />
         </nav>
