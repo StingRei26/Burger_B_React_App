95601e8154b94d6e60a3009582ff493a2d713c12
Reiniel Ravelo <reinielravelo@gmail.com>
2019-10-15 17:08:50 -0400

    update

--- react_burger_app/src/components/Navigation/Toolbar/Toolbar.js
+++ react_burger_app/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,15 @@
 import React from 'react'; 
 import classes from './Toolbar.module.css'; 
 import Logo from '../../Logo/Logo'; 
-import NavigationItems from'../NavigationItems/NavigationItems'
+import NavigationItems from'../NavigationItems/NavigationItems'; 
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'; 
 
 const toolbar = (props) => (
 
     <header className={classes.Toolbar}>
 
-        <div>MENU</div>
+       
+        <DrawerToggle clicked={props.drawerToggleClicked} /> {/*aka menu button*/}
         <Logo height="80%"/>
         <nav className={classes.DesktopOnly}>
         <NavigationItems />
